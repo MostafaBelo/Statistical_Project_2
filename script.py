f74b9dc51e420941429280f061083062ec291bef
Your Name <you@example.com>
2023-12-06 22:25:02 +0200

    some work

--- script.py
+++ script.py
@@ -10,8 +10,37 @@ for i in range(len(titles)):
     cols[titles[i]] = i
 
 data = data[1:]
-for i in range(len(data)):
+n = len(data)
+for i in range(n):
     data[i] = data[i].split(",")
 
 def Get(i, c):
+    global data
     return data[i][cols[c]]
+
+grades = {}
+
+for i in range(n):
+    if (Get(i, "School") in grades):
+        grades[Get(i, "School")][0] += float(Get(i, "Grade"))
+        grades[Get(i, "School")][1] += 1
+    else:
+        grades[Get(i, "School")] = [float(Get(i, "Grade")),1]
+
+# max_school = 0
+grades_sort = []
+for i in grades:
+    grades[i][0] = grades[i][0]/grades[i][1]
+    grades_sort.append((grades[i][0], grades[i][1], i))
+    # if (max_school == 0):
+    #     max_school = i
+
+    # if (grades[i] > grades[max_school]):
+    #     max_school = i
+
+grades_sort.sort()
+
+print(grades_sort)
+
+# print(max_school)
+# 56877, highest mean schools
